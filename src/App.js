47c490cf42d376c62a31ef44f2be384a5497e557
Pusiaczek <lipek8@gmail.com>
2021-08-05 18:47:22 +0200

    cleaned login functions

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
-import { useState, useEffect, useReducer } from 'react';
-import axios from 'axios';
+import { useState, useEffect, useContext } from 'react';
 
-import AuthContext from './store/auth-context';
+import AuthContext, { AuthContextProvider } from './store/auth-context';
+
+
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 
@@ -14,21 +15,12 @@ import SignUp from './Components/PopUp/SignUp';
 import Login from './Components/PopUp/Login';
 
 import getDefaultPosts from './Queries/getDefaultPosts';
-import authReducer from './Queries/authReducer';
-
 
 function App() {
-  // const [isLoggedIn, setIsLoggedIn] = useState();
-  const [loggedInAuth, dispatchLogin] = useReducer(authReducer, {
-    isLoggedIn: false,
-  })
-
-
-  console.log(loggedInAuth);
-  // const { isLoggedIn: loggedIn } = loggedInAuth;
-
   const [posts, setPosts] = useState([]);
   const [showSignUpPopUp, setShowSignUpPopUp] = useState(false);
+  const context = useContext(AuthContext)
+
 
   const closeSignUpPopUp = () => {
     setShowSignUpPopUp(false);
@@ -48,49 +40,16 @@ function App() {
   }, [])
 
 
-  let axiosConfig = {
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    }
-  }
 
-  const loginHandler = async (loginData) => {
-    const user = {
-      ...loginData,
-      ttl: 3600
-    }
-
-    try {
-      const response = await axios.post(
-        'https://akademia108.pl/api/social-app/user/login',
-        JSON.stringify(user),
-        axiosConfig,
-      )
-
-      if (response.data.jwt_token) {
-        dispatchLogin({ type: 'login', token: response.data.jwt_token })
-      } else {
-        throw new Error('brakuja tokena');
-      }
-
-    } catch (error) {
-      console.log("error", error);
-    }
-  }
 
 
   return (
     <Router>
-      <AuthContext.Provider value={{
-        isLoggedIn: loggedInAuth.isLoggedIn,
-        onLogout: () => { dispatchLogin({ type: 'logout' }) },
-        onLogin: loginHandler
-      }}>
+      <AuthContextProvider>
         <Header />
         <Switch>
           <Route exact path='/'>
-            {!loggedInAuth.isLoggedIn && showSignUpPopUp && <SignUp close={closeSignUpPopUp} />}
+            {!context.isLoggedIn && showSignUpPopUp && <SignUp close={closeSignUpPopUp} />}
             <Home postsData={posts} />
           </Route>
           {/* <Route path='/logout'>
@@ -103,7 +62,7 @@ function App() {
 
           </Route>
         </Switch>
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </Router >
   );
 }
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useReducer } from "react";
+import axios from "axios";
+import authReducer from '../Reducers/authReducer';
+
 
 const AuthContext = React.createContext({
     isLoggedIn: false,
+    onLogout: () => { },
+    onLogin: (username, password) => { }
 });
 
+
+
+let axiosConfig = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
+}
+
+
+
+
+export function AuthContextProvider(props) {
+
+    const [loginAuth, dispatchLogin] = useReducer(authReducer, {
+        isLoggedIn: false,
+    })
+
+    const loginHandler = async (loginData) => {
+        const user = {
+            ...loginData,
+            ttl: 3600
+        }
+
+        try {
+            const response = await axios.post(
+                'https://akademia108.pl/api/social-app/user/login',
+                JSON.stringify(user),
+                axiosConfig,
+            )
+
+            if (response.data.jwt_token) {
+                dispatchLogin({ type: 'login', token: response.data.jwt_token })
+            } else {
+                throw new Error('brakuja tokena');
+            }
+
+        } catch (error) {
+            console.log("error", error);
+        }
+    }
+
+    return (
+        < AuthContext.Provider value={{
+            isLoggedIn: loginAuth.isLoggedIn,
+            onLogout: () => { dispatchLogin({ type: 'logout' }) },
+            onLogin: loginHandler
+        }}>
+            {props.children}
+        </AuthContext.Provider >
+    )
+}
+
+
+
+
+
+
 export default AuthContext;
+
+
